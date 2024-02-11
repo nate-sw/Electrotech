b01aa47324e0c9538ab975b601c780b709ec1ba0
nate-sw <nathanaelsimardwhite@gmail.com>
2024-02-11 17:31:39 -0500

    Project update 2

--- rfid_test.asm
+++ rfid_test.asm
@@ -6,27 +6,40 @@
 
 .include "m8515def.inc"
 
-.equ     BUSON = $82
-;.equ     ADCADR = $1000 ; Memory address for ADC
-;.equ     SVSEGADR = $1800 ; Memory address for 7 segment displays
+.cseg
 .equ     ADRR1 = $2000 ; Memory address for LCD's control
 .equ     ADRR2 = $2100 ; Memory address for LCD's data
+
+
 
 init0:
     LDI     R16,LOW(RAMEND)
     OUT     SPL,R16
     LDI     R16,HIGH(RAMEND)
     OUT     SPL,R16
+    RCALL   init_lcd
 
-init_SPI-Master:
-    ;Set MOSI and SCK to output, all others input
-    LDI     R16,(1<<DD_MOSI)|(1<<DD_SCK)
-    OUT     DDR_SPI,R16
+init_spi:
+    ;Set MOSI, SCK and PB0 to output, all others input
+    LDI     R16, $B1 
+    OUT     DDRB,R16
     ;Enable SPI, Master, set clock rate fck/16
     LDI     R16,(1<<SPE)|(1<<MSTR)|(1<<CPHA)|(1<<SPR0)
     OUT     SPCR,R16
 
+init_RFID_main:
+    RCALL   init_rfid
+
+test:
+    RCALL   spi_rfid_ping
+    MOV     R31,R17
+    MOV     R30,R18
+    RCALL lcd_msg_dsp
+
+
+msg0:    .db   $0D, "RFID Tag", $00
 
 .include "delays.inc"
+;.include "startup.inc"
 .include "lcdio.inc"
-.include "startup.inc"
+.include "rfid_io.inc"
