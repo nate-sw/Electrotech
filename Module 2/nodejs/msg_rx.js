3a73aa31f382d221189dbc782710cd7379ae9190
nate-sw <105872836+nate-sw@users.noreply.github.com>
2024-03-24 15:38:29 -0400

    Update M2-1.1

--- Module 2/nodejs/msg_rx.js
+++ Module 2/nodejs/msg_rx.js
@@ -1,25 +1,31 @@
 const Websocket = require('ws');
 const { exec } = require('child_process');
+// Set the appropriate server address and port number here
+const ws = new Websocket('ws://192.168.0.7:6432');
 
-const wss = new Websocket.Server({ port: 6432 });
-
-wss.on('connection', function connection(ws) {
-    console.log('Client connected');
-
-    ws.on('message', function incoming(message) {
-        console.log('Received %s', message);
-
-        exec(`php ../includes/entry.inc.php "${message}"`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error executing PHP script: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`PHP script error: ${stderr}`);
-                return;
-            }
-        });
-
-
-    });
+ws.on('open', function open() {
+    console.log('Connected to WebSocket server');
 });
+
+ws.on('message', function incoming(data) {
+    console.log('Received data from server:', data);
+  
+    // Pass the received data to a PHP script for processing
+    exec(`php ../includes/entry.inc.php "${data}"`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing PHP script: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`PHP script error: ${stderr}`);
+        return;
+      }
+      
+    });
+  });
+
+ws.on('close', function close() {
+    console.log('Connection to WebSocket server closed');
+});
+
+        
